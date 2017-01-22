1d9daa786bdc91e12eafd479ae8d08e96598cf80
whuang8 <huang554@purdue.edu>
2017-01-21 21:36:01 -0500

    Initial commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,24 @@
+import Vapor
+import VaporPostgreSQL
+
+
+let drop = Droplet()
+try drop.addProvider(VaporPostgreSQL.Provider)
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.get("db_version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT Version()")
+        return JSON(version)
+    }
+    return "Error connecting to db"
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
